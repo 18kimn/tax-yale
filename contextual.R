c06467d77ddfe455f1c411f0f66c696e69c9cf32
Nathan Kim <nathanckim18@gmail.com>
2020-03-04 19:46:16 -0500

    added contextual variables from census

--- contextual.R
+++ contextual.R
@@ -5,11 +5,12 @@ library(tidycensus)
 acs_vars <- load_variables(2018, "acs5")
 
 vars_tbl <- acs_vars %>% 
-  filter(str_detect(name, "C15002|B25003|B03002|B19113|C27001|B07004")) %>% 
-  filter((str_detect(name, "C15002") & str_detect(name, "_002|004|006|007|009|011")) | !str_detect(name, "C15002"))
+  filter(str_detect(name, "C15002|B25003|B03002|B17020|C27001|B07004")) %>% 
+  filter((str_detect(name, "C15002") & str_detect(name, "_002|003|006|007|008|011")) | !str_detect(name, "C15002"))
 #aka if it's an education variable filter it for the total, high school grad, and college grad rows
 write_csv(vars_tbl, "data/raw/contextual/variable descriptions.csv")
 
+#filtered out for correct variables and added labels to indicate race
 vars_tbl <- rio::import("data/raw/contextual/formatted vars.xlsx")
 vars <- vars_tbl %>% 
   pull(name)
@@ -53,14 +54,15 @@ edu <- map_dfr(races, function(race){
     pivot_wider(names_from = variable, values_from = estimate)
   
   totm <- str_subset(names(sub), "_002")
-  hsm <- str_subset(names(sub), "_004")
+  nhsm <- str_subset(names(sub), "_003")
   cm <- str_subset(names(sub), "_006")
   totf <- str_subset(names(sub), "_007")
-  hsf <- str_subset(names(sub), "_009")
+  nhsf <- str_subset(names(sub), "_008")
   cf <- str_subset(names(sub), "_011")
-  #overall hs = hsm +hsf / totm + totf
+  #overall hs = 1 -((nhsm+nhsf)/ totm + totf)
+  #aka high school + up is 1 - (those who didn't finish hs)/(total ppl)
   sub <- sub %>% 
-    mutate(hs :=  (!!sym(hsm) + !!sym(hsf))/(!!sym(totm) + !!sym(totf)),
+    mutate(hs :=  1 - (!!sym(nhsm) + !!sym(nhsf))/(!!sym(totm) + !!sym(totf)),
            college :=  (!!sym(cm) + !!sym(cf))/(!!sym(totm) + !!sym(totf))) %>% 
     select(GEOID, race, hs, college)
 })
@@ -83,6 +85,29 @@ ins <- map_dfr(races, function(race){
     mutate(ins = (!!sym(ny) + !!sym(nm) + !!sym(no))/(!!sym(toty)+ !!sym(totm) + !!sym(toto))) %>% 
     select(GEOID, race, ins)
 })
-inc <- context_data %>% filter(race != "T", str_detect(variable, "B19113"))
 
-full_df <- list(ins, inc, )
+#poverty status (income/income per capita/median household income had a lot of missing values)
+
+pov <- map_dfr(races, function(race){
+  sub <- context_data %>% 
+    filter(race == !!race, str_detect(variable, "B17020")) %>%
+    pivot_wider(names_from = variable, values_from = estimate)
+  
+  tot <- str_subset(names(sub), "_001")
+  pov <- str_subset(names(sub), "_002")
+
+  sub <- sub %>% 
+    mutate(pov = !!sym(pov)/!!sym(tot)) %>% 
+    select(GEOID, race, pov)
+})
+
+#raw count by race
+race_df <- context_data %>% 
+  filter(str_detect(variable, "B02001|B03002"), race != "T") %>% 
+  #B03002 is nonhispanic white and ihspanic
+  select(-variable)
+
+contextual_full <- list(edu, pov, ins, mobi, race_df, rent) %>% 
+  reduce(full_join, by = c("GEOID", "race"))
+
+write_csv(contextual_full, "data/clean/contextual variables.csv")
